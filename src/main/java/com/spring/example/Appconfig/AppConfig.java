0d3d17ce1622f27ca5a881bec696c7b12ce6370d
NEYON250 <racheluwihirwe01@gmail.com>
2026-03-13 00:58:26 +0200

    Create Spring configuration class using @Configuration and @ComponentScan

---
+++ src/main/java/com/spring/example/Appconfig/AppConfig.java
@@ -0,0 +1,10 @@
+package com.spring.example.Appconfig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.spring.example")
+public class AppConfig {
+
+}
